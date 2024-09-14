75976cc955a196831c1c4e7996932b53a9094252
PaulAld1980 <hidden80@mail.ru>
2024-09-14 19:17:27 +0500

    first commit

--- nums.py
+++
@@ -1,5 +0,0 @@
-from num2words import num2words
-
-print(num2words(42))  # Выведет forty-two
-print(num2words(42, to='ordinal')) # Выведет forty-second
-print(num2words(42, lang='fr'))  # Выведет quarante-deux
